089d3e9c5a87255f647cff8b1acbb7cb282d81c7
timosauter <sautertimo@t-online.de>
2024-12-20 03:23:02 +0100

    all flags inputcheck

--- src/main.c
+++ src/main.c
@@ -3,8 +3,31 @@
 
 void parse_args(int argc, char *argv[]) {
     for (int i = 1; i < argc; i++) {
-        if (strcmp(argv[i], "-c") == 0) {
-            printf("Flag '-c' erkannt\n");
+        if(strcmp(argv[i], "--help") == 0){
+        printf("--help erkannt");
+        }else if (argv[i][0] == '-') {
+            switch (argv[i][1]) {
+                case 'c':
+                    printf("C erkannt");
+                    break;
+                case 'w':
+                    printf("w erkannt");
+                    break;
+                case 'l':
+                    printf("l erkannt");
+                    break;
+                case 'L':
+                    printf("L erkannt");
+                    break;
+                case 'h':
+                    printf("h erkannt");
+                    break;
+                case '-help':
+                    printf("-help erkannt");
+                    break;
+            }
+        } else {
+                printf("default case flag nicht gefunden");
         }
     }
 }
