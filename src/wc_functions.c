5dbb167361c92ca6a8786601a4e6725d929d9ffb
damokleseses <purpurajin@gmail.com>
2025-02-07 12:15:01 +0100

    chages for wc_functions.c based on linux file api

--- src/wc_functions.c
+++ src/wc_functions.c
@@ -1,37 +1,76 @@
 #include "wc_functions.h"
 #include <stdio.h>
 #include <stdbool.h>
+#include <fcntl.h>
+#include <unistd.h>
 
-int count_lines(FILE *file) {
+#define BUFFER_SIZE 1024
+
+int count_lines(const char *filename) {
+    int fd = open(filename, O_RDONLY);
+    if (fd == -1) {
+        perror("Fehler beim Öffnen der Datei");
+        return -1;
+    }
+
     int lines = 0;
-    char c;
-    while ((c = getc(file)) != EOF) { //EOF sollte für End of File stehen.
-        if (c == '\n') {
-            lines++;
+    char buffer[BUFFER_SIZE];
+    ssize_t bytes_read;
+
+    while ((bytes_read = read(fd, buffer, BUFFER_SIZE)) > 0) {
+        for (ssize_t i = 0; i < bytes_read; i++) {
+            if (buffer[i] == '\n') {
+                lines++;
+            }
         }
     }
+
+    close(fd);
     return lines;
 }
 
-int count_words(FILE *file) {
+int count_words(const char *filename) {
+    int fd = open(filename, O_RDONLY);
+    if (fd == -1) {
+        perror("Fehler beim Öffnen der Datei");
+        return -1;
+    }
+
     int words = 0;
-    char c;
     bool in_word = false;
-    while ((c = getc(file)) != EOF) {
-        if (c == ' ') {
-            in_word = false;
-        } else if (!in_word) {
-            in_word = true;
-            words++;
+    char buffer[BUFFER_SIZE];
+    ssize_t bytes_read;
+
+    while ((bytes_read = read(fd, buffer, BUFFER_SIZE)) > 0) {
+        for (ssize_t i = 0; i < bytes_read; i++) {
+            if (buffer[i] == ' ' || buffer[i] == '\n' || buffer[i] == '\t') {
+                in_word = false;
+            } else if (!in_word) {
+                in_word = true;
+                words++;
+            }
         }
     }
+
+    close(fd);
     return words;
 }
 
-int count_characters(FILE *file) {
-    int letters = 0;
-    while (fgetc(file) != EOF) {
-        letters++;
+int count_characters(const char *filename) {
+    int fd = open(filename, O_RDONLY);
+    if (fd == -1) {
+        perror("Fehler beim Öffnen der Datei");
+        return -1;
     }
-    return letters;
+
+    int characters = 0;
+    char buffer[BUFFER_SIZE];
+    ssize_t bytes_read;
+
+    while ((bytes_read = read(fd, buffer, BUFFER_SIZE)) > 0) {
+        characters += bytes_read;
+    }
+
+    close(fd);
+    return characters;
 }
